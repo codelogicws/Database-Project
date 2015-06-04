988cc5b16243e8589975a1758b5af572a8e2b453
codelogicws <russ@codelogic.ws>
2015-06-04 16:48:47 -0700

    Success connecting with the database

---
+++ src/ws/codelogic/databasetest/Database.java
@@ -0,0 +1,44 @@
+package ws.codelogic.databasetest;
+import java.sql.*;
+import java.util.Scanner;
+
+public class Database {
+
+    public void connect(){
+
+        String password = getPassword();
+
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            String connectionUrl = "jdbc:sqlserver://10.0.0.77\\SQLEXPRESS;" +
+                    "database=codelogic.ws;" +
+                    "user=sa;" +
+                    "password=" + password;
+            Connection con = DriverManager.getConnection(connectionUrl);
+            System.out.println("Connected.");
+
+            String SQL = "SELECT * FROM test";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            while(rs.next()){
+                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
+            }
+        } catch (SQLException e1) {
+            System.out.println("error");
+            e1.printStackTrace();
+        } catch(Exception e){
+            System.out.println("error");
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+
+    }
+
+    public String getPassword(){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter Password");
+        return reader.next();
+    }
+}
---
+++ src/ws/codelogic/databasetest/Main.java
@@ -0,0 +1,8 @@
+package ws.codelogic.databasetest;
+
+public class Main {
+    public static void main(String[] args){
+        Database db = new Database();
+        db.connect();
+    }
+}
